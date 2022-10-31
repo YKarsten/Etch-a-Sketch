92ade9eca087fc72de6f37bea6f39c74f5b98e8a
YKarsten <yannik.karsten@web.de>
2022-10-31 15:58:37 +0100

    Build a reset button that also rebuilds the canvas according to player input

--- index.js
+++ index.js
@@ -1,14 +1,79 @@
-// Create a 16x16 grid of sqaure divs on the webpage
-// Put the square divs into the div.container
-// Pick a method to create the divs using
-// CSS Grid
-// Remove Borders and margins as they adjust the size of the boxes?
+// Add a button to the top of the screen that will send the user a popup 
+// asking for the number of squares per side for the new grid.
+// Once entered, the existing grid should be removed and a new grid 
+// should be generated in the same total space as before (e.g. 960px wide)
+// so that you’ve got a new sketch pad. 
+// Tip: Set the limit for the user input to a maximum of 100.
+// A larger number of squares results in more computer resources being used,
+// potentially causing delays, freezing, or crashing that we want to prevent.
 
-const container = document.querySelector("div");
+
+let userInput;
+const resetButton = document.querySelector("button");
+
+//Specifiy the canvas size.
+function getUserInput(){
+    do {
+        userInput = parseInt(prompt("Please enter the size of the new canvas (max supported size is 100x100"))
+        return userInput;
+    } while (userInput > 100 || 
+             userInput < 2 || 
+             Number.isInteger(userInput) === false);
+}
+
+// Delete the previous canvas
+function resetCanvas() {
+    while (container.firstChild) {
+      container.removeChild(container.lastChild);
+    }
+}
+
+// Draw a new canvas according to the user Input
+function createCanvas(userInput){
+    // container.style.gridTemplateColumns = repeat(userInput, 1fr);
+    // container.style.gridTemplateRows = repeat(userInput, 1fr);
+    for (let i=0; i<(userInput*userInput); i++){
+        let childDiv= document.createElement("div");
+        childDiv.classList.add("childDiv")
+    
+        childDiv.style.backgroundColor ="yellow"
+        childDiv.style.borderStyle="solid"
+        childDiv.style.margin=0;
+        childDiv.textContent=`${i}`
+    
+        container.appendChild(childDiv);
+        childArray[i]=childDiv;
+    }
+    // Give it the desired hover effect
+    childArray.forEach((child) =>{
+        child.addEventListener("mouseover", ()=> {
+            child.style.backgroundColor="black";
+        })
+    })
+}
+
+// Click the button to reset and draw a new canvas according to user input
+resetButton.addEventListener("click",()=>{
+    getUserInput();
+    resetCanvas();
+    createCanvas(userInput);
+}
+)
+
+
+
+
+
+const container = document.querySelector("div")
+// container.style.gridTemplateColumns = repeat(userInput, 1fr);
+// container.style.gridTemplateRows = repeat(userInput, 1fr);
 let childArray=[]
 
-for (let i=0; i<16*16; i++){
+
+//Draw a default canvas
+for (let i=0; i<(256); i++){
     let childDiv= document.createElement("div");
+    childDiv.classList.add("childDiv")
 
     childDiv.style.backgroundColor ="yellow"
     childDiv.style.borderStyle="solid"
@@ -20,16 +85,12 @@ for (let i=0; i<16*16; i++){
     childArray[i]=childDiv;
 }
 
-// console.log(childArray)
-// console.log(childArray.length)
 
-// Set up a “hover” effect so that the grid divs change color 
-// when your mouse passes over them, leaving a (pixelated) trail 
-// through your grid like a pen would.
-
+// Hover effect BW
 childArray.forEach((child) =>{
     child.addEventListener("mouseover", ()=> {
         child.style.backgroundColor="black";
     })
 })
+
 
